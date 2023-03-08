6a533b03722f8f1cfd149eb706827e3860a27825
Nadav Ben Hur <nadavb@moveo.co.il>
2023-03-08 10:28:22 +0200

    shopping edit display added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [new Ingredient('Pepper', 4), new Ingredient('Tomato', 4)]
 }
